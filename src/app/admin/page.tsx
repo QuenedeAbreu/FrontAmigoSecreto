86437e5cd710ab6ea96af87ca851a2ed235b3aef
Quenede Abreu <quenede.in@gmail.com>
2024-04-05 17:38:09 -0500

    add router admin and admin/login

---
+++ src/app/admin/page.tsx
@@ -0,0 +1,14 @@
+import * as api from "@/api/site";
+import { redirect } from "next/navigation";
+import Search from "@/components/site/Search"
+
+export default async function page() {
+
+
+
+  return (
+    <main className="text-center mx-auto max-w-lg p-5">
+      Painel Admin
+    </main>
+  )
+}	
