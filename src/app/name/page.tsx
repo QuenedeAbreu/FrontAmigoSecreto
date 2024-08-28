b0da5d3c99d0d7313a0810171b6f611c2162920d
Quenede Abreu <quenede.in@gmail.com>
2024-08-28 14:42:35 -0500

    *

--- src/app/name/page.tsx
+++ src/app/name/page.tsx
@@ -3,7 +3,7 @@ import * as api from '@/api/server'
 import { redirect } from 'next/navigation';
 import { NamePage } from '@/components/name/NamePage'
 
-export const runtime = 'edge';
+
 export default async function page() {
 
   const logged = await api.isTokenValid();
