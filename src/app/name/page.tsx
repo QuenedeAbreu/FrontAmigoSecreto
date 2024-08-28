01a177cfbf460f293555c6226a6010b102afc728
Quenede Abreu <quenede.in@gmail.com>
2024-08-28 14:46:43 -0500

    *

--- src/app/name/page.tsx
+++ src/app/name/page.tsx
@@ -2,7 +2,7 @@
 import * as api from '@/api/server'
 import { redirect } from 'next/navigation';
 import { NamePage } from '@/components/name/NamePage'
-
+export const runtime = 'edge';
 
 export default async function page() {
 
